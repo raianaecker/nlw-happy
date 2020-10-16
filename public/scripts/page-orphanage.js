1d3f5cfe3f4c4993ace1efcf440b3d67127227f9
Raiana Ecker <61332441+raianaecker@users.noreply.github.com>
2020-10-16 16:25:49 -0300

    Add files via upload

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,37 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    srollWheelZoom: false,
+    zoomControl: false
+}
+
+const map = L.map("mapid", options).setView([-26.2861838,-48.889917], 15); 
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+const icon = L.icon({
+    iconUrl: "/images/map-marker.svg", 
+    iconSize: [58, 68],                         
+    iconAnchor: [29, 68],                     
+    popupAnchor: [170,2] 
+})
+
+L.marker([-26.2861838,-48.889917], { icon })
+.addTo(map)
+
+function selectImage(event) {
+    const button = event.currentTarget
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    }
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+    
+    imageContainer.src = image.src
+    
+    button.classList.add('active')
+}
